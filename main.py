bf1d061cb6e3226d45b8c3f52dc95c23e586fcda
kalaydzhyan <kalaydzhyan@gmail.com>
2017-09-13 18:51:51 -0700

    reproduces angular curve

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from __future__ import division
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
 import numpy as np
-import math, random, sys
-
+from scipy.fftpack import dct
+import math, sys
+from statsmodels.nonparametric.smoothers_lowess import lowess
 
 """ Some global variables """
 mass = 1 # mass of the scalar field, add it later
@@ -11,10 +12,22 @@ number_sources = 100
 observation_time = 1000
 sampling_time = 1
 
+""" Properties of the source frequencies """
+frequency_mean = 0.1
+frequency_variation = 0.01
+
+""" Regime we are probing and angular resolution, in degrees"""
+short_wavelength_regime = True
+delta_zeta = 10
+
+
 def normalize(vector):
     norm = math.sqrt(math.fsum([v**2 for v in vector]))
-    return [element/norm for element in vector] 
-
+    if norm == 0:
+        print "Null vector! Can't normalize."
+        
+    return [element/norm for element in vector]
+     
 # Source class 
 class Source:
     
@@ -25,7 +38,7 @@ class Source:
         
         """ Random seed for testing purposes """
         if values is not None:
-            random.seed(values)
+            np.random.seed(values)
         
     def __repr__(self):
         """string representation of the object"""
@@ -49,11 +62,11 @@ class Source:
         else:
             name = set_name
             
-        amplitude   = random.random()
-        frequency   = random.random()/2
-        sin_delta   = (random.random() - 0.5) * 2
-        alpha       = 2 * math.pi * random.random()
-        phase       = 2 * math.pi * random.random()
+        amplitude   = 10 * np.random.random()
+        frequency   = np.random.normal(frequency_mean, frequency_variation)
+        sin_delta   = (np.random.random() - 0.5) * 2
+        alpha       = 2 * math.pi * np.random.random()
+        phase       = 2 * math.pi * np.random.random()
         
         cos_delta   = math.sqrt(1 - sin_delta ** 2)
         wave_vector = [cos_delta * math.cos(alpha), \
@@ -86,24 +99,35 @@ class Source:
                      one_source['phase'])
         
         return background
+
+# Detector class     
+class Detector:
     
-#    def plot sphere of sources
-#    def remove source by name
+    def __init__(self, values=None):
+        
+        """ Detector position """
+        if values is None:
+            self.position = [0 for _ in range(3)]
+        else:
+            self.position = values
 
 def Fourier(data, sampling_interval):
-    
+    """ Fourier transform, conventions: X_k = Sum_m[x_m*Exp[-2 PI i m k / N]] """
     t, y = zip(*data)
 
     N = len(y)                  # number of data points
     k = np.arange(N)
     T = N * sampling_interval
     frq = k/T                   # total frequency range
-    frq = frq[range(N//2)]      # cut by Kotel'nikov
+    Y = np.fft.rfft(y)/N        # real FFT computing and normalization
 
-    Y = np.fft.fft(y)/N         # FFT computing and normalization
-    Y = Y[range(N//2)]
-    
-    return zip(frq, abs(Y))
+    return zip(frq, Y)
+
+def angle(X, Y):
+    """ Angle between two vectors """
+    return np.arccos(np.inner(normalize(X), normalize(Y)))
+
+
 
 print "\n\n"    
 print "--------------------------------------------------------"
@@ -111,39 +135,136 @@ print " Simulation of stochastic backgrounds of various fields "
 print "--------------------------------------------------------"
 print "\nNumber of sources: " + str(number_sources)
 
-""" Creating sources. For reproducible results call Source(0) """
+""" Creating sources. For reproducible results call Source(some_number) """
 sources = Source()
 for source_number in range(number_sources): 
     sources.add()
 
 """ Plotting the sources """
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d', aspect=1.0)
 ax.scatter(sources.positions(0), sources.positions(1), sources.positions(2), c='r', marker='o')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 plt.xticks(np.arange(-1, 1, .5))
 plt.yticks(np.arange(-1, 1, .5))
-plt.title('Positions of sources')
+plt.title('Angular positions of the sources')
 plt.show()
 
-""" Getting data for one detector """
-data = [(t, sources.signal(t, [0, 0, 1])) \
+""" Getting data for the reference detector """
+detector0 = Detector()
+data = [(t, sources.signal(t, detector0.position)) \
         for t in np.arange(0, observation_time, sampling_time)]
 
-""" Signal for one detector """
+""" Signal for the reference detector """
 timeline, signal = zip(*data)
 plt.plot(timeline, signal, '-')
-plt.xlabel('Time (s)')
-plt.ylabel('signal')
-plt.title('Signal for one detector')
+plt.xlabel('t (seconds)')
+plt.ylabel('X(t)')
+plt.title('Signal for the reference detector')
 plt.show()
 
 """ Spectrum of the signal """
 frequencies, fourier_image = zip(*Fourier(data, sampling_time))
-plt.plot(frequencies, fourier_image,'r-') # plotting the spectrum
-plt.xlabel('Freq (Hz)')
-plt.ylabel('Spectrum')
-plt.title('Spectrum of the signal')
+plt.plot(frequencies, np.abs(fourier_image),'r-') 
+plt.xlabel('f (Hz)')
+plt.ylabel('$\sqrt{S_X(f)}$')
+plt.title('Power spectrum of the signal for the reference detector')
 plt.show()
+
+""" Let's add two more detectors """
+detector1 = Detector([1, 0, 0])
+data1 = [(t, sources.signal(t, detector1.position)) \
+        for t in np.arange(0, observation_time, sampling_time)]
+
+detector2 = Detector([np.sin(0.3), np.cos(0.3), 0])
+data2 = [(t, sources.signal(t, detector2.position)) \
+        for t in np.arange(0, observation_time, sampling_time)]
+
+zeta = angle(detector1.position, detector2.position)
+print 'Angle between two detectors = ' + str(np.rad2deg(zeta)) + ' degrees'
+
+""" Compute cross-correlation """
+_, signal1 = zip(*data1)
+_, signal2 = zip(*data2)
+signal1 = np.array(signal1) - np.array(signal)
+signal2 = np.array(signal2) - np.array(signal)
+xcorrelator = np.correlate(signal1, signal2, 'same')/observation_time
+# figure out what is the appropriate regime here
+
+tau = range(0, len(xcorrelator), sampling_time)
+
+plt.plot(tau, xcorrelator, '-')
+plt.xlabel('t (seconds)')
+plt.ylabel('x-corr(t)')
+plt.title('correlation')
+plt.show()
+
+#frequencies, x_fourier_image = zip(*Fourier(zip(tau, xcorrelator), sampling_time))
+#_, d1_fourier_image = zip(*Fourier(zip(timeline, signal1), sampling_time))
+#filtered = lowess(x_fourier_image, frequencies, is_sorted=True, frac=0.025, it=0)
+#plt.plot(frequencies, x_fourier_image,'r-', frequencies, filtered, 'b-')
+frequencies, signal1image = zip(*Fourier(zip(timeline, signal1), sampling_time))
+frequencies, signal2image = zip(*Fourier(zip(timeline, signal2), sampling_time))
+
+x_fourier_image = signal1image * np.conj(signal2image)
+
+plt.plot(frequencies, np.real(x_fourier_image),'r-')
+plt.xlabel('f (Hz)')
+plt.ylabel('$S_c(f)$')
+plt.title('Cross-spectrum')
+plt.show()
+
+
+""" Produce the angular curve """
+ratios = []
+
+if short_wavelength_regime:
+    distance1 = 300
+    distance2 = 400
+else:
+    distance1 = 0.001
+    distance2 = 0.0001
+
+zetas = np.arange(0, math.pi, np.deg2rad(delta_zeta))
+timeline = np.arange(0, observation_time, sampling_time)
+
+for zeta in zetas:
+    detector1 = Detector([distance1, 0, 0])
+    signal1 = [sources.signal(t, detector1.position) \
+               for t in timeline]
+
+    detector2 = Detector([distance2 * np.cos(zeta), distance2 * np.sin(zeta), 0])
+    signal2 = [sources.signal(t, detector2.position) \
+               for t in timeline]
+
+    """ Compare to the reference detector in the origin """
+    signal1 = np.array(signal1) - np.array(signal)
+    signal2 = np.array(signal2) - np.array(signal)
+        
+    frequencies, signal1image = zip(*Fourier(zip(timeline, signal1), sampling_time))
+    frequencies, signal2image = zip(*Fourier(zip(timeline, signal2), sampling_time))
+    
+    x_fourier_image = signal1image * np.conj(signal2image)
+    
+    if frequency_variation*observation_time < 1:
+        print 'Frequency variation must be larger than observation time, cannot resolve!'
+    
+    left_f = int((frequency_mean - frequency_variation) * observation_time)
+    right_f = int((frequency_mean + frequency_variation) * observation_time)
+    cross_spectrum_f = np.real(np.array(x_fourier_image))
+    S_cross = np.mean(cross_spectrum_f[left_f:right_f])
+    detector1_spectrum_f = np.abs(np.array(signal1image))**2
+    S_d1 = np.mean(detector1_spectrum_f[left_f:right_f])
+
+    ratios.append(S_cross/S_d1)
+    
+plt.plot(zetas, ratios,'go-')
+
+plt.xlabel('$\zeta$, rad')
+plt.title('Angular curve')
+plt.show()
+
+if short_wavelength_regime:
+    print 'Level position: ' + str(np.mean(ratios)) + ' +/- ' + str(np.var(ratios))
